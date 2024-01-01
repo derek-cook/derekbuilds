ffd063b8ae04f9eb06243a2b1b75a4d87893f3a2
derek-cook <dcook693@gmail.com>
2023-12-31 21:41:20 -0800

    minor ui changes

--- src/app/pool/[[...topic]]/page.tsx
+++ src/app/pool/[[...topic]]/page.tsx
@@ -6,6 +6,9 @@ import { Input } from "~/components/ui/Input";
 
 export default function Pool({ params }: { params: { topic: string } }) {
   const router = useRouter();
+
+  // TODO: connect to realtime for a channel, send messages, etc
+
   return (
     <div className="flex flex-grow flex-col space-y-8 p-4">
       <div className="flex items-center">
@@ -19,9 +22,18 @@ export default function Pool({ params }: { params: { topic: string } }) {
         </Button>
         <h1 className="align-middle text-3xl">{params.topic}</h1>
       </div>
-      <div id="messages-container" className="container flex-grow text-white">
-        <p className="bg-opacity inline rounded-lg rounded-bl-none bg-slate-500 p-2">
-          Hello here's some text
+      <div
+        id="messages-container"
+        className="flex-grow space-y-2 px-3 text-white"
+      >
+        <p className="bg-opacity w-fit rounded-lg rounded-bl-none bg-slate-500 p-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          quibusdam maiores praesentium sit eius repellendus deleniti,
+          accusantium magnam, cum, quis amet. Quae beatae, consequatur veniam
+          nemo ab quas saepe enim!
+        </p>
+        <p className="bg-opacity w-fit rounded-lg rounded-bl-none bg-slate-500 p-2">
+          thats crazy
         </p>
       </div>
       <form className="space-y-2" action="">
