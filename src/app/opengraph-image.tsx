8fd5c4ca93b3a0a987cec8fb7dc8eb029d3a9b56
derek-cook <dcook693@gmail.com>
2024-01-29 02:15:50 -0800

    readd og

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "Derek Builds AI";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default function Image() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          backgroundImage: `url("/opengraph-image.png")`,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {` Stuff I've been working on`}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  );
+}
